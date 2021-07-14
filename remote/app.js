2593369cfc53e94952c66a598af9ba509c5400fd
unknown <nvduc712@gmail.com>
2021-07-14 17:50:52 +0900

    First commit

---
+++ remote/app.js
@@ -0,0 +1,18 @@
+const https = require('https');
+const fs = require('fs');
+const app = require('express')();
+
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+
+const server = https.createServer(options, app);
+
+//routing file
+app.get('/:namefile', function (req,res) {
+    return res.status(200).sendFile(`${__dirname}/`+req.params.namefile);
+});
+
+app.use(require('express').json());
+server.listen(3000, () => { console.log('listening on 3000') });
